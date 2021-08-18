bf56f4ceb46b3d8f278fbc85db14fe860da26fe8
Qiyuan <congqiyuan@gmail.com>
2021-08-18 19:23:22 +0800

    nuc

---
+++ NUC/online_test.py
@@ -0,0 +1,133 @@
+import serial
+import os
+import numpy as np
+import time
+import threading
+import tensorflow as tf
+import softskin
+import IRCamera
+from Network import train_network
+from Driver import ControlOdometryDriver as CD
+import cv2 as cv
+
+if __name__ == "__main__":
+  win_width = 10
+  tf_model = train_network.FrontFollowing_Model(win_width=win_width)
+  weight_path = "./Network/checkpoints/FrontFollowing"
+  tf_model.model.load_weights(weight_path)
+
+  skin = softskin.SoftSkin()
+  IRCamera = IRCamera.IRCamera()
+  skin.build_base_line_data()
+
+  max_ir = 55
+  min_ir = 10
+
+  def binarization(img):
+      """according to an average value of the image to decide the threshold"""
+      if len(img.shape) == 2:
+          threshold = max(img.mean() + 1.4, 23)
+          img[img < threshold] = 0
+          img[img >= threshold] = 1
+      return img
+
+  def filter(img):
+      img_new = np.copy(img)
+      img_new = img_new.reshape((24, 32))
+      filter_kernel = np.ones((2, 2)) / 4
+      """other filters"""
+      # filter_kernel = np.array([[1,1,1],[1,1,1],[1,1,1]])/10
+      # filter_kernel = np.array([[-1, -1, -1], [-1, 8, -1], [-1, -1, -1]])
+      for j in range(1):
+          img_new = cv.filter2D(img_new, -1, filter_kernel)
+      img_new = img_new.flatten()
+      return img_new
+
+
+  ir_threshold = 27
+  ir_data_width = 768
+  skin_data_width = 32
+  buffer_length = win_width
+  buffer = np.zeros((buffer_length * (ir_data_width + skin_data_width), 1))
+
+  thread_skin = threading.Thread(target=skin.read_and_record,args=())
+  thread_skin.start()
+  cd = CD.ControlDriver()
+  # thread_control_driver = threading.Thread(target=cd.control_part, args=())
+  # thread_control_driver.start()
+
+
+  while True:
+      # present_time = time.time()
+      IRCamera.get_irdata_once()
+      if len(IRCamera.temperature) == 768:
+          normalized_temperature = np.array(IRCamera.temperature).reshape((ir_data_width,1))
+          idx = normalized_temperature < ir_threshold
+          normalized_temperature[idx] = 0
+          idx = normalized_temperature >= ir_threshold
+          normalized_temperature[idx] = 1
+          # normalized_temperature = (normalized_temperature-min_ir)/(max_ir-min_ir)
+          buffer[0:(buffer_length-1)*ir_data_width,0] = buffer[ir_data_width:buffer_length*ir_data_width,0]
+          buffer[(buffer_length-1)*ir_data_width:buffer_length*ir_data_width] = normalized_temperature
+
+
+          """skin part start index"""
+          SSI = buffer_length*ir_data_width
+
+          buffer[SSI:SSI+(buffer_length-1)*skin_data_width,0] = \
+              buffer[SSI+skin_data_width:SSI+buffer_length*skin_data_width,0]
+          buffer[SSI+(buffer_length-1)*skin_data_width:SSI+buffer_length*skin_data_width] = \
+              np.array(skin.temp_data).reshape((skin_data_width,1))
+          # buffer.shape = (4000,1)
+          buffer[SSI:SSI + buffer_length * skin_data_width, 0] = 0
+
+          predict_buffer = buffer.reshape((-1,buffer_length*(ir_data_width+skin_data_width),1))
+          result = tf_model.predict(predict_buffer)
+          max_result = result.max()
+          # print(max_result)
+          if max_result == result[0, 0]:
+              print("still!")
+              cd.speed = 0
+              cd.omega = 0
+              cd.radius = 0
+          elif max_result == result[0, 1]:
+              print("forward!")
+              cd.speed = 0.1
+              cd.omega = 0
+              cd.radius = 0
+          elif max_result == result[0, 2]:
+              print("turn left!")
+              cd.speed = 0
+              cd.omega = 0.1
+              cd.radius = 2
+          elif max_result == result[0, 3]:
+              print("turn right!")
+              cd.speed = 0
+              cd.omega = -0.1
+              cd.radius = 2
+          elif max_result == result[0, 4]:
+              print("yuandi left")
+              cd.speed = 0
+              cd.omega = 0.2
+              cd.radius = 0
+          elif max_result == result[0, 5]:
+              print("yuandi right")
+              cd.speed = 0
+              cd.omega = -0.2
+              cd.radius = 0
+          elif max_result == result[0, 6]:
+              print("backword")
+              cd.speed = -0.1
+              cd.omega = 0
+              cd.radius = 0
+          # print(1/(time.time()-present_time))
+          # present_time = time.time()
+
+
+
+
+
+
+
+
+
---
+++ NUC/record_all.py
@@ -0,0 +1,88 @@
+import IMU
+import IRCamera
+import softskin
+import time
+import leg_detector
+import numpy as np
+import serial
+import threading
+import os,sys
+from Driver import ControlOdometryDriver as CD
+
+resource = os.path.abspath(
+    os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".." + os.path.sep +"record_irdata"+
+    os.path.sep + "data")
+
+"""portal num"""
+camera_portal = '/dev/ttyUSB2'
+lidar_portal = '/dev/ttyUSB0'
+IMU_walker_portal = '/dev/ttyUSB3'
+
+
+"""IMU part"""
+# IMU_human = IMU.IMU()
+# IMU_human.open_serial("/dev/ttyUSB1")
+IMU_walker = IMU.IMU()
+IMU_walker.open_serial(IMU_walker_portal)
+"""Camera part"""
+Camera = IRCamera.IRCamera()
+"""skin part"""
+Skin = softskin.SoftSkin()
+"""control driver(record mode)"""
+Cd = CD.ControlDriver(record_mode=True)
+"""leg detector part"""
+Ld = leg_detector.Leg_detector(lidar_portal)
+"""initiate skin part"""
+time.sleep(1)
+Skin.build_base_line_data()
+
+seperately_recording = True
+
+
+if seperately_recording:
+    thread_skin = threading.Thread(target=Skin.read_and_record, args=(True,))
+    thread_camera = threading.Thread(target=Camera.record_write, args=(True, True, True))
+    # thread_IMU_human = threading.Thread(target=IMU_human.read_record,args=())
+    thread_IMU_walker = threading.Thread(target=IMU_walker.read_record, args=())
+    thread_cd = threading.Thread(target=Cd.control_part, args=())
+    thread_leg = threading.Thread(target=Ld.scan_procedure, args=(False,True))
+
+    thread_skin.start()
+    thread_camera.start()
+    # thread_IMU_human.start()
+    thread_IMU_walker.start()
+    thread_cd.start()
+    thread_leg.start()
+
+else:
+    pass
+    # thread_skin = threading.Thread(target=Skin.read_and_record, args=())
+    # # thread_IMU_human = threading.Thread(target=IMU_human.read_record,args=())
+    # thread_IMU_walker = threading.Thread(target=IMU_walker.read_record, args=())
+    # thread_cd = threading.Thread(target=Cd.control_part, args=())
+    # thread_leg = threading.Thread(target=Ld.main_procedure, args=())
+    #
+    # thread_skin.start()
+    # # thread_IMU_human.start()
+    # thread_IMU_walker.start()
+    # thread_cd.start()
+    # thread_leg.start()
+    #
+    # file_path = resource + os.path.sep + "all_data.txt"
+    # file_data = open(file_path, "w")
+    # while True:
+    #     # present_time = time.time()
+    #     Camera.get_irdata_once()
+    #     data = []
+    #     if len(Camera.temperature) == 768:
+    #         time_index = time.time()
+    #         data.append(time_index)
+    #         data = data + Camera.temperature
+    #         data = data +
+    #         pass
+
+
+
+
+
+
