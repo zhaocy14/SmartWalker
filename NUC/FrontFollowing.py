912e3510b8e91207106427ed2da8de9b01d640c7
gwz <guowz@connect.hku.hk>
2021-09-03 16:15:54 +0800

    9/3

--- NUC/FrontFollowing.py
+++ NUC/FrontFollowing.py
@@ -67,8 +67,8 @@ def main_FFL(CD: cd.ControlDriver, LD: Leg_detector.Leg_detector):
                 CD.speed = 0
                 # CD.omega = 0.15
                 # CD.radius = 80
-                CD.omega = 0.1*abs((1+(current_position[1] -left_boundry)*0.25))
-                CD.radius= 82*abs((1-(current_position[1] -left_boundry)*0.02))
+                CD.omega = 0.2*abs((1+(current_position[1] -left_boundry)*0.25))
+                CD.radius= 75*abs((1-(current_position[1] -left_boundry)*0.02))
                 str1 = "left"
                 time.sleep(0.2)
             elif current_position[5] < center_right_boundry \
@@ -77,8 +77,8 @@ def main_FFL(CD: cd.ControlDriver, LD: Leg_detector.Leg_detector):
                 CD.speed = 0
                 # CD.omega = -0.15
                 # CD.radius = 80
-                CD.omega = -0.1*abs((1+(right_boundry-current_position[3] )*0.25))
-                CD.radius = 80*abs((1-(right_boundry-current_position[3] )*0.02))
+                CD.omega = -0.2*abs((1+(right_boundry-current_position[3] )*0.25))
+                CD.radius = 75*abs((1-(right_boundry-current_position[3] )*0.02))
                 str1 = "right"
                 time.sleep(0.2)
             else:
