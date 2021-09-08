0ab27ab9abd758accc048f430c54c2a1eeff3fc6
zhaocy14 <522653608@qq.com>
2021-09-08 12:29:04 +0800

    nuc

--- NUC/record_all.py
+++ NUC/record_all.py
@@ -45,7 +45,7 @@ if seperately_recording:
     # thread_IMU_human = threading.Thread(target=IMU_human.read_record,args=())
     thread_IMU_walker = threading.Thread(target=IMU_walker.read_record, args=())
     thread_cd = threading.Thread(target=Cd.control_part, args=())
-    thread_leg = threading.Thread(target=Ld.scan_procedure, args=(False, True))
+    thread_leg = threading.Thread(target=Ld.scan_procedure, args=(resource,False, True))
 
     thread_skin.start()
     thread_camera.start()
