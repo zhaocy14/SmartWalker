0fc1372d82f9e5406b43684c9a87904368cdc3f0
52265 <522653608@qq.com>
2021-10-11 16:11:36 +0800

    change voltage to distance

--- Sensors/Infrared_Sensor.py
+++ Sensors/Infrared_Sensor.py
@@ -3,6 +3,7 @@ import serial.tools.list_ports
 import numpy as np
 import os,sys
 import time
+
 import matplotlib.pyplot as plt
 
 pwd = os.path.abspath(os.path.abspath(__file__))
@@ -58,6 +59,10 @@ class Infrared_Sensor(object):
         self.average_weight = np.ones((1,self.buffer_length))/self.buffer_length
         # status: whether the sensor is out of range
         self.status = np.zeros((1,self.sensor_num))
+        # table: first row is voltage, second row is distance
+        self.table = [[20, 30, 40, 50, 60, 70, 80, 90, 100, 110, 120, 130, 140, 150],
+                      [2.5, 2, 1.55, 1.25, 1.1, 0.85, 0.8, 0.73, 0.7, 0.65, 0.6, 0.5, 0.45, 0.4]]
+        self.table = np.array(self.table)
         pass
 
     def check_stability(self):
@@ -69,6 +74,24 @@ class Infrared_Sensor(object):
             self.status[0,i] = np.mean((self.buffer[:,i]-mean[i])**2)
         self.status = np.sqrt(self.status)
         print("status:",self.status)
+
+    def my_inter(self,x,x0,x1,y0,y1):
+        return (x-x1)/(x0-x1)*y0 + (x-x0)/(x1-x0)*y1
+
+    def turn_to_distance(self):
+        for i in range(self.sensor_num):
+            for i in range(self.table.shape[1]):
+                if self.distance_data[j] >= self.table[1,i]:
+                    break
+            if i == 0:
+                self.distance_data[j] = 20
+            elif i == self.table.shape[1]:
+                self.distance_data[j] = 150
+            else:
+                self.distance_data[j] = self.my_inter(self.distance_data[j],self.table[1,i-1],self.table[1,i],
+                                                      self.table[0,i-1],self.table[0,i])
+
+
 
     def read_data(self, is_shown:bool=False, is_record:bool=False, is_average:bool=False):
         # current_time = time.time()
@@ -97,7 +120,8 @@ class Infrared_Sensor(object):
                     # one_line_data = list(map(int, one_line_data))
                         self.distance_data = self.buffer[-1,:]
                     # change the value into real voltage:
-                    self.distance_data = self.distance_data/1024
+                    self.distance_data = self.distance_data/1024*5
+
 
                     # print(self.raw_data, type(self.raw_data), type(self.raw_data[0]))
                     if is_shown:
