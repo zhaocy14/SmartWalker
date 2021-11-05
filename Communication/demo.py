9bc611f4415276722b0a43b5a371c608d067f0b1
Owen Yip <me@owenyip.com>
2021-11-05 15:40:33 +0800

    add demo

--- Communication/demo.py
+++ Communication/demo.py
@@ -3,7 +3,6 @@
 # Author: Owen Yip
 # Mail: me@owenyip.com
 #
-from datetime import datetime
 import os,sys
 pwd = os.path.abspath(os.path.abspath(__file__))
 father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
@@ -16,29 +15,10 @@ import json
 
 context = zmq.Context()
 
-def transmit_demo():
-  socket = context.socket(zmq.PUB)
-  socket.bind("tcp://*:5454")
-
-  while True:
-      msg = datetime.now().strftime("%H:%M:%S")
-      socket.send_string(msg)
-      print("Sending data: %s" % msg)
-      time.sleep(1)
-
-def transmit():
-  socket = context.socket(zmq.PUB)
-  socket.bind("tcp://*:5454")
-
-  while True:
-      msg = json.dumps('''position data here''')
-      socket.send_string(msg)
-      print("Sending data: %s" % msg)
-
 
 def receive():
   socket = context.socket(zmq.SUB)
-  socket.connect("tcp://127.0.0.1:5455")
+  socket.connect("tcp://127.0.0.1:5454")
   topicfilter = ""
   socket.setsockopt_string(zmq.SUBSCRIBE, topicfilter)
 
@@ -49,4 +29,4 @@ def receive():
           print("Received request: %s" % message)
 
 if __name__ == "__main__":
-    transmit_demo()
+    receive()
