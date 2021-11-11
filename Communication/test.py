b363ea0141346806e81fc2c96d6b4c6977ca978e
Owen Yip <me@owenyip.com>
2021-11-11 13:44:14 +0800

    clean up folder space

--- Communication/test.py
+++ Communication/test.py
@@ -5,8 +5,13 @@ father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
 sys.path.append(father_path)
 
 from Communication.Cpp_command import CppCommand
+from Communication.Receiver import Receiver
 cco = CppCommand.get_instance()
 
 if __name__ == "__main__":
     # cco.start_navigation(mode="offline", testing="local", stdout=True)
-    cco.start_navigation(stdout=True)
+    cco.start_navigation(stdout=False)
+    recvObj = Receiver()
+    # recvObj.start_DriverControl()
+    for _ in recvObj.start_Pose():
+        print("testing", _)
