8c5c2044b0f413ef9017ccf8b677dff220eff0cb
Owen Yip <me@owenyip.com>
2021-11-11 11:40:41 +0800

    fix conflict

--- Communication/test_sensors.py
+++ Communication/test_sensors.py
@@ -5,7 +5,7 @@ father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
 sys.path.append(father_path)
 
 from Communication.Cpp_command import CppCommand
-cco = CppCommand.get_instance(lidar_port="/dev/ttyUSB0", imu_port="/dev/ttyUSB4")
+cco = CppCommand.get_instance(lidar_port="/dev/ttyUSB0", imu_port="/dev/ttyUSB2")
 
 if __name__ == "__main__":
     cco.start_sensors(stdout=True)
