f52030b09a5ac77fd905c933944b4ec98bf78c37
zhaocy14 <522653608@qq.com>
2021-11-15 13:19:13 +0800

    add cpp command example

---
+++ Communication/example.py
@@ -0,0 +1,31 @@
+import os, sys
+pwd = os.path.abspath(os.path.abspath(__file__))
+father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
+sys.path.append(father_path)
+
+from Communication.Cpp_command import CppCommand
+from Communication.Receiver import Receiver
+cco = CppCommand.get_instance(lidar_port="/dev/ttyUSB2", imu_port="/dev/ttyUSB1")
+
+if __name__ == "__main__":
+    # cco.start_navigation(mode="offline", testing="local", stdout=False, map_file="map.2021-09-25.070808")
+    
+    '''
+        Start map drawing example
+        **If wanted to use the latest map file, can use map_file="latest"
+    '''
+    cco.start_navigation(stdout=False, driver_ctrl=False, ir_sensor=False, map_file="map.2021-09-25.070808")
+    recvObj = Receiver(mode="local")
+    # recvObj.start_DriverControl()
+    for _ in recvObj.start_Pose():
+        print("testing", _)
+    
+    '''
+        Start map drawing example
+    '''
+    # cco.start_drawing(stdout=True)
+    
+    '''
+        Start sensors command example
+    '''
+    # cco.start_sensors(stdout=True)
