ffdb85d3d53aa6c9ef6499e886d09596f67d0ee4
52265 <522653608@qq.com>
2021-11-15 16:20:46 +0800

    test lidar

--- random_test.py
+++ random_test.py
@@ -1,5 +1,3 @@
-def my_inter(x, x0, x1, y0, y1):
-    return (x - x1) / (x0 - x1) * y0 + (x - x0) / (x1 - x0) * y1
-print(my_inter(0.42,0.4,0.45,150,140))
-# d = tf.math.scalar_mul(a,b)
-# print(c)
+import numpy as np
+while True:
+     print(1)
