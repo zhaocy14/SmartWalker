34dec1cb90a9fae53b35981039e4e44e7319faeb
Owen Yip <me@owenyip.com>
2022-01-09 20:56:52 +0800

    fix transmit issue

--- Communication/Modules/Transmit.py
+++ Communication/Modules/Transmit.py
@@ -1,7 +1,6 @@
 import zmq
 from global_variables import WalkerPort
 
-
 class TransmitZMQ(object):
     _instance = None
 
@@ -24,15 +23,7 @@ class TransmitZMQ(object):
         self.socket = context.socket(zmq.PUB)
         self.socket.bind("tcp://{}:{}".format(address, port))
         print('Transmit initialized')
-    
-    @staticmethod
-    def get_instance(address="*", port="5455"):
-        if TransmitZMQ._instance is None:
-            TransmitZMQ(address=address, port=port)
-        return TransmitZMQ._instance
-    
-    def get_id(self):
-        return self._id
+
     
     def send(self, topic=None, msg=None):
         if topic is not None and msg is not None:
