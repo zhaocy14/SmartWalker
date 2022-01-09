3faa5425afac7cbc4acf03a08b8d8082e642d281
Owen Yip <me@owenyip.com>
2022-01-09 19:08:35 +0800

    add back the requirements

---
+++ Communication/example.py
@@ -0,0 +1,45 @@
+import os, sys
+pwd = os.path.abspath(os.path.abspath(__file__))
+father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
+sys.path.append(father_path)
+import time
+
+from Communication.Cpp_command import CppCommand
+from Communication.Receiver import Receiver
+import Communication.State_client as csc
+from global_variables import WalkerState
+# cco = CppCommand.get_instance(lidar_port="/dev/ttyUSB2", imu_port="/dev/ttyUSB1")
+# cco = CppCommand.get_instance()
+if __name__ == "__main__":
+    '''C++ Demo'''
+    with CppCommand.get_instance() as cco:
+        # cco.start_navigation(mode="offline", testing="local", stdout=False, map_file="map.2021-09-25.070808")
+        '''
+            Start navigation example
+            **If wanted to use the latest map file, can use map_file="latest"
+            **If needed to move the walker, set driver_ctrl=True
+            **If needed the ir sensor, set ir_sensor=True
+        '''
+        # cco.start_navigation(stdout=False, driver_ctrl=False, ir_sensor=False, map_file="map.2021-09-25.070808")
+        # time.sleep(5)
+        # recvObj = Receiver(mode="local")
+        # recvObj.start_DriverControl()
+        # for _ in recvObj.start_Pose():
+        #     print("testing", _)
+
+        '''
+            Start map drawing example
+        '''
+        # cco.start_drawing(stdout=True)
+
+        '''
+            Start sensors command example
+        '''
+        # cco.start_sensors(stdout=False)
+        # cco.start_drawing(stdout=True)
+        print("C++ ended")
+        
+    '''State Client Demo'''
+    state_client = csc.StateClient.get_instance()
+        # print(state_client.change_walker_state(WalkerState.IDLE.CHARGING))
+    print(WalkerState.IDLE.CHARGING is WalkerState.IDLE.CHARGING)
