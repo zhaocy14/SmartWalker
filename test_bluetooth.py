172e5cce1a4026f1d2607a0be4eee608b27456d7
Owen Yip <me@owenyip.com>
2022-01-17 13:32:12 +0800

    add bluetooth llinux

--- test_bluetooth.py
+++ test_bluetooth.py
@@ -5,12 +5,6 @@
 # Mail: me@owenyip.com
 #
 
-"""PyBluez simple example rfcomm-server.py
-Simple demonstration of a server application that uses RFCOMM sockets.
-Author: Albert Huang <albert@csail.mit.edu>
-$Id: rfcomm-server.py 518 2007-08-10 07:20:07Z albert $
-"""
-
 import bluetooth
 
 server_sock = bluetooth.BluetoothSocket(bluetooth.RFCOMM)
@@ -20,7 +14,7 @@ server_sock.listen(1)
 
 port = server_sock.getsockname()[1]
 
-uuid = "94f39d29-7d6d-437d-973b-fba39e49d4ee"
+uuid = "4df03521-6912-431d-89c7-5b3b63a648be"
 
 bluetooth.advertise_service(server_sock, "SampleServer", service_id=uuid,
                             service_classes=[uuid, bluetooth.SERIAL_PORT_CLASS],
