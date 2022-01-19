a0c73b6298c1677249ec3d4789eaa5546fe061ed
Owen Yip <me@owenyip.com>
2022-01-19 18:54:45 +0800

    update the test

--- Communication/test.py
+++ Communication/test.py
@@ -4,17 +4,19 @@ pwd = os.path.abspath(os.path.abspath(__file__))
 father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
 sys.path.append(father_path)
 
-from Communication.Cpp_command import CppCommand
-from Communication.Receiver import Receiver
+# from Communication.Cpp_command import CppCommand
+# from Communication.Receiver import Receiver
 
-cco = CppCommand.get_instance()
+from Communication.Modules.Time_transmit import TimeTransmit
 
 if __name__ == "__main__":
-    cco.start_navigation(mode="offline", testing="local", stdout=False)
-    # cco.start_navigation(stdout=False)
-    recvObj = Receiver(mode="local")
-    # recvObj.start_DriverControl()
-    for _ in recvObj.start_Pose():
-        print("testing", _)
+    # cco.start_navigation(mode="offline", testing="local", stdout=False)
+    # # cco.start_navigation(stdout=False)
+    # recvObj = Receiver(mode="local")
+    # # recvObj.start_DriverControl()
+    # for _ in recvObj.start_Pose():
+    #     print("testing", _)
+    time_sock = TimeTransmit("Time")
+    time_sock.start()
     
         
