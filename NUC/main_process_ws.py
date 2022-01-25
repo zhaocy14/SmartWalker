3760b120e584b114e70f36f9708a7dab2ea2dd87
Forence <wang00sheng@gmail.com>
2022-01-25 21:41:38 +0800

    synchronize

--- NUC/main_process_ws.py
+++ NUC/main_process_ws.py
@@ -191,7 +191,7 @@ if __name__ == '__main__':
     
     MappingMicro = False
     isDebug = True
-    useCD = True
+    useCD = False
     left_right = 0
     SSL_Event = multiprocessing.Event()
     SSL_Event.set()  # TODO: for debugging
