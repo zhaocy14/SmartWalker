736a6673e4c0e7cfa583a47074f9271e0306501c
52265 <522653608@qq.com>
2022-02-09 04:02:51 +0800

    main procedure update

--- Sensors/LiDAR.py
+++ Sensors/LiDAR.py
@@ -7,65 +7,30 @@ import sys
 import time
 import matplotlib.pyplot as plt
 import rplidar
-
-pwd = os.path.abspath(os.path.abspath(__file__))
-father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
-sys.path.append(father_path)
-data_path = os.path.abspath(
-    os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".." +
-    os.path.sep + "data")
-
-
-def print_serial(port):
-    print("---------------[ %s ]---------------" % port.name)
-    print("Path: %s" % port.device)
-    print("Descript: %s" % port.description)
-    print("HWID: %s" % port.hwid)
-    if not None == port.manufacturer:
-        print("Manufacture: %s" % port.manufacturer)
-    if not None == port.product:
-        print("Product: %s" % port.product)
-    if not None == port.interface:
-        print("Interface: %s" % port.interface)
-    if not None == port.vid:
-        print("Vid:",port.vid)
-    if not None == port.pid:
-        print("Pid:",port.pid)
-    print()
-
-
-def detect_serials(description="target device", vid=0x10c4, pid=0xea60):
-    ports = serial.tools.list_ports.comports()
-    for port in ports:
-        print_serial(port)
-        if port.description.__contains__(description):
-            port_path = port.device
-            return port_path
-        else:
-            print("Cannot find the target device: %s" % description)
-    return None
+from Sensors.SensorConfig import *
+from Sensors.SensorFunctions import *
 
 
 class lidar(object):
 
     def __init__(self,is_zmq:bool=False):
         super().__init__()
-        self.port_name = detect_serials(description="CP2102 USB")
+        self.port_name = detect_serials(port_key=LIDAR_DISCRIPTION, sensor_name="LiDAR")
         if not is_zmq:
-            self.lidar_0 = rplidar.RPLidar(self.port_name)
+            self.python_lidar = rplidar.RPLidar(self.port_name)
         else:
-            self.lidar_0 = []
+            self.python_lidar = []
         self.scan_data_list = []
 
     def rplidar_scan_procedure(self,is_show:bool=False):
         # present_time = time.time()
         while True:
             # try:
-                info = self.lidar_0.get_info()
-                health = self.lidar_0.get_health()
+                info = self.python_lidar.get_info()
+                health = self.python_lidar.get_health()
                 print(info)
                 print(health)
-                for i, scan in enumerate(self.lidar_0.iter_scans(max_buf_meas=5000)):
+                for i, scan in enumerate(self.python_lidar.iter_scans(max_buf_meas=5000)):
                     self.scan_data_list = scan
                     if is_show:
                         print(self.scan_data_list)
--- Sensors/SensorConfig.py
+++ Sensors/SensorConfig.py
@@ -1,13 +1,28 @@
-import os,sys
+import os, sys
 
 #   DATA PATH
 pwd = os.path.abspath(os.path.abspath(__file__))
 father_path = os.path.abspath(os.path.dirname(pwd) + os.path.sep + "..")
 sys.path.append(father_path)
-DATA_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".."+ os.path.sep + "data")
+DATA_path = os.path.abspath(os.path.dirname(os.path.abspath(__file__)) + os.path.sep + ".." + os.path.sep + "data")
+print(sys.path)
+
+"""PORT Location or description"""
+CAMERA_LOCATION = "3.2-1"
+IMU_LOCATION = "3.2-1"
+STM32_LOCATION = "3.2.3"
+LIDAR_DISCRIPTION = "CP2102 USB"
 
 #   Speaker USB port
 SPEAKER_LOCATION_1 = "3.2-1"
 SPEAKER_LOCATION_2 = "3.2-2"
 SPEAKER_LOCATION_3 = "3.2-3"
 SPEAKER_LOCATION_4 = "3.2-4"
+
+#   Speaker Command
+PLAY_SONG = b'\xAA\x07\x02\x00'
+PLAY = b'\xAA\x02\x00\xAC'
+MIC_INI_TIME_GAP = 1 # unit:second
+MAX_VOLUME = 30
+MIN_VOLUME = 1
+VOLUME_SET = b'\xAA\x13\x01'
