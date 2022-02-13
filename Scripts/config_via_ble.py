ce3493502f1b1e36bcc33db4a270fa4140b06865
Owen Yip <owen.yip@dreamover-studio.com>
2022-02-14 02:37:54 +0800

    add wifi module

--- Scripts/config_via_ble.py
+++ Scripts/config_via_ble.py
@@ -20,11 +20,14 @@
 
 import logging
 import random
+import json
 
 # Bluezero modules
 # from bluezero import async_tools
 from bluezero import adapter
 from bluezero import peripheral
+
+from wifi_connector import WifiConnector
 
 # constants
 # Custom service uuid
@@ -36,94 +39,103 @@ SetWifiControlPoint = "b7e9e754-1e59-42d4-810f-16ed47244b50"
 WIFI_FMT_DSCP = '2904'
 
 
-def read_value():
-    """
-    Example read callback. Value returned needs to a list of bytes/integers
-    in little endian format.
-
-    This one does a mock reading CPU temperature callback.
-    Return list of integer values.
-    Bluetooth expects the values to be in little endian format and the
-    temperature characteristic to be an sint16 (signed & 2 octets) and that
-    is what dictates the values to be used in the int.to_bytes method call.
-
-    :return: list of uint8 values
-    """
-    cpu_value = random.randrange(3200, 5310, 10) / 100
-    return list(int(cpu_value * 100).to_bytes(2,
-                                              byteorder='little', signed=True))
-
-
-def update_value(characteristic):
-    """
-    Example of callback to send notifications
-
-    :param characteristic:
-    :return: boolean to indicate if timer should continue
-    """
-    # read/calculate new value.
-    new_value = read_value()
-    # Causes characteristic to be updated and send notification
-    characteristic.set_value(new_value)
-    # Return True to continue notifying. Return a False will stop notifications
-    # Getting the value from the characteristic of if it is notifying
-    return characteristic.is_notifying
-
-
-def write_value(value, options):
-    print("Received: ", bytes(value).decode('utf-8'))
-    return True
-
-
-# def notify_callback(notifying, characteristic):
-#     """
-#     Noitificaton callback example. In this case used to start a timer event
-#     which calls the update callback ever 2 seconds
-
-#     :param notifying: boolean for start or stop of notifications
-#     :param characteristic: The python object for this characteristic
-#     """
-#     if notifying:
-#         async_tools.add_timer_seconds(2, update_value, characteristic)
-
-
-def main(adapter_address):
-    """Creation of peripheral"""
-    logger = logging.getLogger('localGATT')
-    logger.setLevel(logging.DEBUG)
-    # Example of the output from read_value
-    print('Advertising SmartWalker BLE')
-    # Create peripheral
-    ble_configurator = peripheral.Peripheral(adapter_address,
-                                        local_name='SmartWalker BLE Configurator')
-    # Add service
-    ble_configurator.add_service(srv_id=1, uuid=SmartWalkerUUID, primary=True)
-    # Add characteristic
-    # ble_configurator.add_characteristic(srv_id=1, chr_id=1, uuid=SetWifiControlPoint,
-    #                                value=[], notifying=False,
-    #                                flags=['read', 'notify'],
-    #                                read_callback=read_value,
-    #                                write_callback=None,
-    #                                notify_callback=notify_callback
-    #                                )
+class BleManager:
+    def __init__(self, address=None):
+        if address is None:
+            self.adpater_address = list(adapter.Adapter.available())[0].address
+        else:
+            self.adpater_address = address
     
-    ble_configurator.add_characteristic(srv_id=1, chr_id=1, uuid=SetWifiControlPoint,
-                                   value=[], notifying=False,
-                                   flags=['write'],
-                                   read_callback=None,
-                                   write_callback=write_value,
-                                   notify_callback=None
-                                   )
-    # Add descriptor
-    # ble_configurator.add_descriptor(srv_id=1, chr_id=1, dsc_id=1, uuid=WIFI_FMT_DSCP,
-    #                            value=[0x0E, 0xFE, 0x2F, 0x27, 0x01, 0x00,
-    #                                   0x00],
-    #                            flags=['read'])
-    # Publish peripheral and start event loop
-    ble_configurator.publish()
+    
+    def read_value(self):
+        """
+        Example read callback. Value returned needs to a list of bytes/integers
+        in little endian format.
+
+        This one does a mock reading CPU temperature callback.
+        Return list of integer values.
+        Bluetooth expects the values to be in little endian format and the
+        temperature characteristic to be an sint16 (signed & 2 octets) and that
+        is what dictates the values to be used in the int.to_bytes method call.
+
+        :return: list of uint8 values
+        """
+        cpu_value = random.randrange(3200, 5310, 10) / 100
+        return list(int(cpu_value * 100).to_bytes(2,
+                                                byteorder='little', signed=True))
+
+
+    def wificonfig_notify_value(self, characteristic):
+        """
+        Example of callback to send notifications
+
+        :param characteristic:
+        :return: boolean to indicate if timer should continue
+        """
+        # read/calculate new value.
+        new_value = self.read_value()
+        # Causes characteristic to be updated and send notification
+        characteristic.set_value(new_value)
+        # Return True to continue notifying. Return a False will stop notifications
+        # Getting the value from the characteristic of if it is notifying
+        return characteristic.is_notifying
+
+
+    def wificonfig_write_value(self, value, options):
+        if value:
+            print("Received: ", bytes(value).decode('utf-8'))
+            wifi_connector = WifiConnector()
+            wifi_config = json.loads(bytes(value).decode('utf-8'))
+            print("parse: ", wifi_config)
+            result = wifi_connector.Connect(ssid=wifi_config["ssid"], password=wifi_config["password"]["value"])
+            print("connect result: ", result)
+            
+        return True
+
+
+    # def notify_callback(notifying, characteristic):
+    #     """
+    #     Noitificaton callback example. In this case used to start a timer event
+    #     which calls the update callback ever 2 seconds
+
+    #     :param notifying: boolean for start or stop of notifications
+    #     :param characteristic: The python object for this characteristic
+    #     """
+    #     if notifying:
+    #         async_tools.add_timer_seconds(2, update_value, characteristic)
+
+
+    def start(self):
+        """Creation of peripheral"""
+        logger = logging.getLogger('localGATT')
+        logger.setLevel(logging.DEBUG)
+        # Example of the output from read_value
+        print('Advertising SmartWalker BLE')
+        # Create peripheral
+        ble_configurator = peripheral.Peripheral(self.adapter_address,
+                                            local_name='SmartWalker BLE Configurator')
+        
+        # Add service
+        ble_configurator.add_service(srv_id=1, uuid=SmartWalkerUUID, primary=True)
+        # Add characteristic
+        ble_configurator.add_characteristic(srv_id=1, chr_id=1, uuid=SetWifiControlPoint,
+                                    value=[], notifying=False,
+                                    flags=['write', 'notify'],
+                                    read_callback=None,
+                                    write_callback=self.wificonfig_write_value,
+                                    notify_callback=self.wificonfig_notify_value
+                                    )
+        # Add descriptor
+        # ble_configurator.add_descriptor(srv_id=1, chr_id=1, dsc_id=1, uuid=WIFI_FMT_DSCP,
+        #                            value=[0x0E, 0xFE, 0x2F, 0x27, 0x01, 0x00,
+        #                                   0x00],
+        #                            flags=['read'])
+        # Publish peripheral and start event loop
+        ble_configurator.publish()
 
 
 if __name__ == '__main__':
     # Get the default adapter address and pass it to main
     # print(list(adapter.Adapter.available())[0].address)
-    main(list(adapter.Adapter.available())[0].address)
+    # main(list(adapter.Adapter.available())[0].address)
+    ble_manager = BleManager()
--- Scripts/wifi_connector.py
+++ Scripts/wifi_connector.py
@@ -5,112 +5,114 @@
 #
 import wifi
 
+class WifiConnector:
+    def Search(self):
+        wifilist = []
 
-def Search():
-    wifilist = []
+        cells = wifi.Cell.all('wlan0')
 
-    cells = wifi.Cell.all('wlan0')
+        for cell in cells:
+            wifilist.append(cell)
 
-    for cell in cells:
-        wifilist.append(cell)
-
-    return wifilist
-
-
-def FindFromSearchList(ssid):
-    wifilist = Search()
-
-    for cell in wifilist:
-        if cell.ssid == ssid:
-            return cell
-
-    return False
+        return wifilist
 
 
-def FindFromSavedList(ssid):
-    cell = wifi.Scheme.find('wlan0', ssid)
+    def FindFromSearchList(self, ssid):
+        wifilist = self.Search()
 
-    if cell:
-        return cell
-
-    return False
-
-
-def Connect(ssid, password=None, hidden=False):
-    if not hidden:
-        cell = FindFromSearchList(ssid)
-
-        if cell:
-            savedcell = FindFromSavedList(ssid)
-
-            # Already Saved from Setting
-            if savedcell:
-                savedcell.activate()
+        for cell in wifilist:
+            if cell.ssid == ssid:
                 return cell
 
-            # First time to conenct
-            else:
-                if cell.encrypted:
-                    if password:
-                        scheme = Add(cell, password)
+        return False
+
+
+    def FindFromSavedList(self, ssid):
+        cell = wifi.Scheme.find('wlan0', ssid)
+
+        if cell:
+            return cell
+
+        return False
+
+
+    def Connect(self, ssid, password=None, hidden=False):
+        if not hidden:
+            cell = self.FindFromSearchList(ssid)
+
+            if cell:
+                savedcell = self.FindFromSavedList(ssid)
+
+                # Already Saved from Setting
+                if savedcell:
+                    savedcell.activate()
+                    return cell
+
+                # First time to conenct
+                else:
+                    if cell.encrypted:
+                        if password:
+                            scheme = self.Add(cell, password)
+
+                            try:
+                                scheme.activate()
+
+                            # Wrong Password
+                            except wifi.exceptions.ConnectionError:
+                                self.Delete(ssid)
+                                return False
+
+                            return cell
+                        else:
+                            return False
+                    else:
+                        scheme = self.Add(cell)
 
                         try:
                             scheme.activate()
-
-                        # Wrong Password
                         except wifi.exceptions.ConnectionError:
-                            Delete(ssid)
+                            self.Delete(ssid)
                             return False
 
                         return cell
-                    else:
-                        return False
-                else:
-                    scheme = Add(cell)
-
-                    try:
-                        scheme.activate()
-                    except wifi.exceptions.ConnectionError:
-                        Delete(ssid)
-                        return False
-
-                    return cell
-    
-    else:
-        print('This is a hidden network')
-    
-    return False
-
-
-def Add(cell, password=None):
-    if not cell:
+        
+        # Todo: Hidden network handling
+        else:
+            print('This is a hidden network')
+        
         return False
 
-    scheme = wifi.Scheme.for_cell('wlan0', cell.ssid, cell, password)
-    scheme.save()
-    return scheme
+
+    def Add(self, cell, password=None):
+        if not cell:
+            return False
+
+        scheme = wifi.Scheme.for_cell('wlan0', cell.ssid, cell, password)
+        scheme.save()
+        return scheme
 
 
-def Delete(ssid):
-    if not ssid:
+    def Delete(self, ssid):
+        if not ssid:
+            return False
+
+        cell = self.FindFromSavedList(ssid)
+
+        if cell:
+            cell.delete()
+            return True
+
         return False
-
-    cell = FindFromSavedList(ssid)
-
-    if cell:
-        cell.delete()
-        return True
-
-    return False
 
 
 if __name__ == '__main__':
+    wifi_connector = WifiConnector()
     # Search WiFi and return WiFi list
-    print(Search())
+    print(wifi_connector.Search())
 
     # Connect WiFi with password & without password
-    print(Connect('OpenWiFi'))
-    print(Connect('ClosedWiFi', 'password'))
+    print(wifi_connector.Connect('OpenWiFi'))
+    print(wifi_connector.Connect('ClosedWiFi', 'password'))
 
     # Delete WiFi from auto connect list
-    print(Delete('DeleteWiFi'))
+    print(wifi_connector.Delete('DeleteWiFi'))
